8cfdf77108467b05e71e2758fdecf079ccdebf77
Caitlin Mothes <ccmothes@gmail.com>
2021-09-15 14:02:54 -0600

    edit getPM25 function

---
+++ utils/getPM25_ccm_tract.R
@@ -0,0 +1,47 @@
+# function to process pm2.5 indicator
+
+library(dplyr)
+library(vroom)
+library(sf)
+library(stringr)
+
+
+# now create function to get data for each geometry
+
+getPM25 <- function(geometry){
+  
+  #read in and clean data
+  d <- vroom("data/2017_pm25_daily_average.txt.gz") %>% 
+    #filter just colorado tracts
+    filter(str_starts(FIPS, "08")) %>%
+    #concentration was read in as character
+    mutate(Conc = as.numeric(`pm25_daily_average(ug/m3)`)) %>% 
+    group_by(FIPS) %>% 
+    summarise(pm25_median = median(Conc)) %>% 
+    #rename as tract for calculation below
+    dplyr::select(tract = FIPS, pm25_median)
+  
+    
+  #when geometry is tract or block level...
+  if(nchar(geometry$GEOID[1]) >= nchar(d$GEOID[1])) {
+    
+    #add tract-level column to use as join then keep original geoid (tract or block)
+    pm25 <- as.data.frame(geometry) %>% mutate(tract = str_sub(GEOID, start = 1, end = 11)) %>% 
+      left_join(d, by = "tract") %>% dplyr::select(GEOID, pm25 = pm25_median) 
+    
+  } else {
+    
+    # when geometry is county level.. just cut FIPS to county level and group by that
+   pm25 <-  d %>% mutate(GEOID = str_sub(tract, start = 1, end = 5)) %>% 
+      group_by(GEOID) %>% 
+      summarise(pm25 = median(pm25_median))
+    
+  }
+  
+  return(pm25)
+  
+}
+
+
+
+
